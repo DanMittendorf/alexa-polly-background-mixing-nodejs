280cb704c39581886397e77b67ccdc30c03bc7b6
Daniel Mittendorf <daniel@digivoice.io>
2019-04-09 10:17:52 +0200

    version #2 with a few changes
    Simplified usage of function. Check if file already exists (uses md5 hash of polly ssml + background sound). If file exists in s3 bucket, load it from there. Otherwise generate a new polly/background mixing mp3. Also checks if sox/lame files are already present in /tmp/.
    
    All of this is in order to increase efficiency and speed.
    
    Usage of function is now easier than ever:
    
    let pollyVoiceWithBackground = await generatePollyUrl(ssml_string, voiceName, background_sound_file);
    
    No need to choose a polly_filename and mixing_result filename. Those will be generated automatically.

--- lambda/eu-west-1_ProofOfConcept-PollyWithBackground/index.js
+++ lambda/eu-west-1_ProofOfConcept-PollyWithBackground/index.js
@@ -7,6 +7,7 @@ const aws = require('aws-sdk'); //for s3 saving
 const lambdaAudio = require('lambda-audio'); //magic
 const mp3Duration = require('mp3-duration'); //to calculate polly audio length
 const fs = require('fs-extra'); //required for local file write and read
+const crypto = require('crypto'); //for md5 hashing ssml string to generate a checksume filename for caching
 
 // settings
 const myRegion = 'eu-west-1' // edit for your desired region
@@ -82,27 +83,38 @@ const putObject = (myBucket, key, body, contentType) =>
 const mix_polly_with_background = (background_mp3, polly_voice_mp3, resulting_mp3, duration) => 
 lambdaAudio.sox ('-m '+background_mp3+' '+polly_voice_mp3+' -C 48.01 '+resulting_mp3+' rate 22050 gain -l 16 trim 0 '+duration).then(() => {
         return resulting_mp3
-        
 }).catch(err => console.error("mix error: "+err));
 
-/** 
- * This is where the magic happens
+/** This is where the magic happens
  * ssml: <speak>Text</speak>
  * voice: name of polly voice: https://docs.aws.amazon.com/de_de/polly/latest/dg/voicelist.html)
  * background_sound: see background_sfx! define audio files stored locally in your lambda "audio" folder @ 48kb/s / 22.050 Hz
  * polly_voice: temporary polly voice filename for saving in lambda /tmp/
  * sound_mix_result: filename for resulting mix of voice+background, will be saved in s3 bucket! see settings!
  **/
-async function generatePollyUrl (ssml, voice, background_sound, polly_voice, sound_mix_result) {
-    await copyFiles(); //has to invoke this function in order to copy sox / lame to /tmp/ to be able to execute them later. this has to happen every time because the tmp folder get purged every few minutes - todo: implement check if files exist and have the correct permissions +x
-    const pollyVoice = await generatePollyAudio(ssml, voice); 
-    await fs.outputFile('/tmp/'+polly_voice, pollyVoice.AudioStream); //writes pollyAudioStream to writeable /tmp/ folder
-    const duration = await mp3Duration('/tmp/'+polly_voice); //calculate length of polly voice. this is important for mixing result because mixing of 5 seconds polly with 10 seconds background will result in 10 seconds polly + background. but you only want the background sfx to be as long as the polly voice
-    var file = await mix_polly_with_background (background_sound, '/tmp/'+polly_voice, '/tmp/'+sound_mix_result, duration); //mixes background with polly and saves to tmp folder, limited by duration of polly voice
-    const uploadFile = await fs.readFile(file); //read the file
-    var writeToS3 = await writeAudioStreamToS3Bucket(uploadFile, sound_mix_result); 
-    console.log(writeToS3.url);
-  return writeToS3.url;
+async function generatePollyUrl (ssml, voice, background_sound) {
+  let sound_mix_result = crypto.createHash('md5').update(voice+background_sound).digest('hex')+".mp3"; //create a standard filename based on ssml voice and background music  greate a universal md5 hash for shorter filename
+  console.log("sound mix result filename: "+sound_mix_result);
+
+  try { // first: checking if file exists, if not, do the magic
+    await s3.headObject({Bucket: myBucket,Key: sound_mix_result}).promise();
+    console.log("requested file exists in your s3 bucket. returning the url to the audio tag now.")
+    return '<audio src="https://s3-'+myRegion+'.amazonaws.com/'+myBucket+'/'+sound_mix_result+'"/>';;
+  } catch (err) { // error case: file does not exist.
+      console.log("File does not exist. So generating it now." + err);
+      let polly_voice = "polly_tmp_"+Math.round(+new Date() / 10)+".mp3"; //generate a temp filename for polly mp3. will be purged in /tmp/ soon
+      console.log("polly voice filename: "+polly_voice);
+      if (fs.existsSync('/tmp/sox') && fs.existsSync('/tmp/lame')) { console.log('Found lame and sox file'); }
+      else { await copyFiles(); }//has to invoke this function in order to copy sox / lame to /tmp/ to be able to execute them later. this has to happen every time because the tmp folder get purged every few minutes - todo: implement check if files exist and have the correct permissions +x
+      const pollyVoice = await generatePollyAudio(ssml, voice); 
+      await fs.outputFile('/tmp/'+polly_voice, pollyVoice.AudioStream); //writes pollyAudioStream to writeable /tmp/ folder
+      const duration = await mp3Duration('/tmp/'+polly_voice); //calculate length of polly voice. this is important for mixing result because mixing of 5 seconds polly with 10 seconds background will result in 10 seconds polly + background. but you only want the background sfx to be as long as the polly voice
+      var file = await mix_polly_with_background (background_sound, '/tmp/'+polly_voice, '/tmp/'+sound_mix_result, duration); //mixes background with polly and saves to tmp folder, limited by duration of polly voice
+      const uploadFile = await fs.readFile(file); //read the file
+      var writeToS3 = await writeAudioStreamToS3Bucket(uploadFile, sound_mix_result); 
+      console.log(writeToS3.url);
+    return '<audio src="'+writeToS3.url+'"/>';
+  }
 }
 /**
  * The End*/
@@ -119,12 +131,8 @@ const LaunchHandler = {
   async handle(handlerInput) { //important make it ASYNC
     console.log("LaunchHandler: isHandling ");
     
-    let background_sound = background_sfx; //defines background mp3 file found in the /audio/ folder in lambda function
-    let sound_mix_result = "mixing-result_0.mp3"; //filename of soon to be mix result
-    let polly_voice = "pollyvoice_tmp.mp3"; //tmp polly filename
-
-    let pollyUrl = await generatePollyUrl("<speak>Another test with Polly voice Brian. Do you like my male voice? I certainly do like my fantastic voice! It's even greater with ocean waves in the background!</speak>", "Brian", background_sound, polly_voice, sound_mix_result);
-    let easySpeakOutput = 'And here comes Brians polly voice with background sounds: <audio src="'+pollyUrl+'"/> Wow, that is so cool! ';
+    let pollyVoiceWithBackground = await generatePollyUrl("<speak>Another test with Polly voice Brian. Do you like my male voice? I certainly do like my fantastic voice! It's even greater with ocean waves in the background!</speak>", "Brian", background_sfx);
+    let easySpeakOutput = 'And here comes Brians polly voice with background sounds: '+ pollyVoiceWithBackground +' Wow, that is so cool! ';
  
     
     return handlerInput.responseBuilder
